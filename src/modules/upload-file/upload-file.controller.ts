1935a77861e8e7d1d82fb4735da83163b1799b1d
Adrian Costa <adriancosta1215@gmail.com>
2025-01-21 22:58:28 -0300

    feat: GCP bucket upload image

--- src/modules/upload-file/upload-file.controller.ts
+++ src/modules/upload-file/upload-file.controller.ts
@@ -1,43 +1,80 @@
 import { Request, Response } from 'express';
-import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3"
 import crypto from 'crypto'
-
-const bucketName = process.env.AWS_BUCKET_NAME
-const region = process.env.AWS_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
-
-const s3Client = new S3Client({
-  region,
-  accessKeyId,
-  secretAccessKey
-})
-
-const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex')
+const {Storage} = require('@google-cloud/storage');
 
 class Controller {
   public async upload(req: Request, res: Response) {
-    const url = req.file?.location ?? null;
-    
-    const fileName = generateFileName()
-    const uploadParams = {
-      Bucket: bucketName,
-      Body: req.file?.buffer,
-      Key: fileName,
-      ContentType: req.file?.mimetype
+    try {
+      // Mapeamento de MIME para extensão
+      const mimeToExtension: { [key: string]: string } = {
+        'image/jpeg': '.jpg',
+        'image/png': '.png',
+        'image/heic': '.heic',
+        'image/heif': '.heif',
+      };
+
+      const bucketName = 'videos-grupo-fera';
+      const mimeType = req.file!.mimetype; // Obtém o tipo MIME do arquivo
+
+      // Verifica se o tipo MIME é permitido
+      if (!mimeToExtension[mimeType]) {
+        return res.status(400).send({ error: 'Tipo de arquivo não suportado. Permitido: .jpg, .png, .heic, .heif.' });
+      }
+
+      // Gera o nome do arquivo com a extensão correta
+      const fileExtension = mimeToExtension[mimeType];
+      const uniqueFileName = `profile-pictures/${crypto.randomBytes(16).toString('hex')}${fileExtension}`;
+
+      const storage = new Storage({ keyFilename: process.env.GCP_JSON });
+      const bucket = storage.bucket(bucketName);
+      const file = bucket.file(uniqueFileName);
+
+      // Salva o arquivo
+      const stream = file.createWriteStream();
+
+      stream.on('error', (err) => {
+        console.error('Erro ao criar o arquivo:', err);
+        res.status(500).send({ error: 'Erro ao criar o arquivo.' });
+      });
+
+      stream.on('finish', async () => {
+        try {
+          await file.makePublic();
+          const publicUrl = `https://storage.googleapis.com/${bucketName}/${uniqueFileName}`;
+          console.log(`Arquivo criado e disponível em: ${publicUrl}`);
+          res.status(200).send({ message: 'Arquivo criado com sucesso.', publicUrl });
+        } catch (err) {
+          console.error('Erro ao tornar o arquivo público:', err);
+          res.status(500).send({ error: 'Erro ao tornar o arquivo público.' });
+        }
+      });
+
+      stream.end(req.file!.buffer);
+    } catch (error) {
+      console.error('Erro inesperado:', error);
+      res.status(500).send({ error: 'Erro interno do servidor.' });
     }
-
-    await s3Client.send(new PutObjectCommand(uploadParams));
-    const resp2 = await s3Client.send(new GetObjectCommand({
-      Bucket: bucketName,
-      Key: fileName
-    }))
-
-    const awsUrl = `https://grupofera.s3.sa-east-1.amazonaws.com/${fileName}`
-
-    res.send({ awsUrl }).status(200)
-
   }
 }
+    // const url = req.file?.location ?? null;
+    
+    // const fileName = generateFileName()
+    // const uploadParams = {
+    //   Bucket: bucketName,
+    //   Body: req.file?.buffer,
+    //   Key: fileName,
+    //   ContentType: req.file?.mimetype
+    // }
+
+    // await s3Client.send(new PutObjectCommand(uploadParams));
+    // const resp2 = await s3Client.send(new GetObjectCommand({
+    //   Bucket: bucketName,
+    //   Key: fileName
+    // }))
+
+    // const awsUrl = `https://grupofera.s3.sa-east-1.amazonaws.com/${fileName}`
+
+    // res.send({ awsUrl }).status(200)
+
 
 export default new Controller();
